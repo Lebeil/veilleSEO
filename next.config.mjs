9d561939a1523b24cc96e19f15fc84fbdbc5d58e
Liedel <liedel75@gmail.com>
2024-09-12 21:25:10 +0200

    fix bug referrer policy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Appliquer à toutes les routes
+        headers: [
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin', // Vous pouvez modifier cette valeur si besoin
+          },
+        ],
+      },
+    ];
+  },
   images: {
     unoptimized: true,
   },
